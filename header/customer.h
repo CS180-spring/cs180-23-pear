947db021e5d57b9d72c5103edcb7b87420bb4c7b
Pranoy Giri Kallikadavil <pranoygiri@gmail.com>
2023-04-27 12:56:14 -0700

    cleaned up main

--- header/customer.h
+++ header/customer.h
@@ -14,6 +14,18 @@ class Customer {
         string phoneNumber;
         int planId;
         string paymentMethod;
+        string sname;
+        string address;
+        string payment;
+        string temp;
+        int id;
+        int tracker;
+        string num;
+        int planId;
+        int choice = -1;
+        int choice_two = -1;
+        vector<Customer> customerDB;
+        
     public:
         Customer();
         Customer(int id, string name, string address, string phoneNumber, int planId, string paymentMethod);
@@ -33,6 +45,10 @@ class Customer {
         void setPaymentMethod(string paymentMethod);
 
         void display() const;
+        void EditCustomer();
+        void AddNewCustomer();
+        void ViewCustomer();
+        void startMenu();
 
 };
 
--- src/test.cpp
+++ src/test.cpp
@@ -1,37 +1,31 @@
 #include "gtest/gtest.h"
-#include "../header/Filter.h"
-#include "../header/Task.h"
-#include "../header/TaskList.h"
-#include "../header/Goal.h"
-#include "../header/Menu.h"
-#include "../testing/View_test.hpp"
+#include "../header/customer.h"
 #include<vector>
 using namespace std;
 
-CustomerList testList = CustomerList("Test");
+Customer testList = Customer("Test");
 static std::stringstream output2;
 vector<Customer> allLists;
-Customer *staticCustomer = new Customer("Essay", "For HIST 108", 1, 20,"07/01/20", "Summer 2020", false, 2);
+Customer *staticCustomer = new Customer(234156, "Trevor","", 9515588585, 24324, "Master");
 Customer *customer2 = new Customer("Study Guide", "For CS 100", 1, 3,"03/11/22", "Winter 2022", false, 0);
-Customer *customer3 = new Customer("Lab 3", "For CS 111", 1, 20,"03/01/22", "Winter 2022", false, 2);
 
 TEST(TaskConstructorTest, defaultConstructor){
     Customer *test = new Customer();
-    EXPECT_EQ(test->Goal::getName(), "");
-    EXPECT_EQ(test->getId(), "561252");
+    EXPECT_EQ(test->getId(), 561252);
+    EXPECT_EQ(test->getName(), "");
     EXPECT_EQ(test->getAddress(), "");
-    EXPECT_EQ(test->getPhoneNumber(), "9515444343");
-    EXPECT_EQ(test->getPlanId(), "12222");
+    EXPECT_EQ(test->getPhoneNumber(), 9515444343);
+    EXPECT_EQ(test->getPlanId(), 12222);
     EXPECT_EQ(test->getPaymentMethod(), "Visa");
     delete test;
 }
 
 int main(int argc, char **argv) {
-    Customer *customer1 = new Customer("Lecture Videos", "For CS 100", 2, 10,"03/19/22", "Winter 2022", false, 5);
-    testList.addCustomer(customer1);
+    Customer *customer1 = new Customer(561252, "Arthur","", 9515444343, 12222, "Visa");
+    testList.AddNewCustomer();
     ::testing::InitGoogleTest(&argc, argv);
     int val = RUN_ALL_TESTS();
-    delete customer1; delete customer2; delete customer3; delete staticCustomer; 
+    delete customer1; delete customer2; delete staticCustomer; 
     allLists.clear();
     return val;
 }

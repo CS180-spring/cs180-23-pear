03c8fddb2211f36847b2d3c9b184a6bf927bdf3f
Pranoy Giri Kallikadavil <pranoygiri@gmail.com>
2023-04-27 15:32:45 -0700

    fixed some of unit tests

--- src/test.cpp
+++ src/test.cpp
@@ -3,29 +3,27 @@
 #include<vector>
 using namespace std;
 
-Customer testList = Customer("Test");
+Customer testList = Customer();
 static std::stringstream output2;
 vector<Customer> allLists;
-Customer *staticCustomer = new Customer(234156, "Trevor","", 9515588585, 24324, "Master");
-Customer *customer2 = new Customer("Study Guide", "For CS 100", 1, 3,"03/11/22", "Winter 2022", false, 0);
+Customer staticCustomer = Customer(234156, "Trevor","", "9515588585", 24324, "Master");
+Customer customer2 = Customer(33345554, "Josue","", "9512234398", 11345, "Visa");
 
 TEST(TaskConstructorTest, defaultConstructor){
-    Customer *test = new Customer();
-    EXPECT_EQ(test->getId(), 561252);
-    EXPECT_EQ(test->getName(), "");
-    EXPECT_EQ(test->getAddress(), "");
-    EXPECT_EQ(test->getPhoneNumber(), 9515444343);
-    EXPECT_EQ(test->getPlanId(), 12222);
-    EXPECT_EQ(test->getPaymentMethod(), "Visa");
-    delete test;
+    Customer test = Customer(234156, "Trevor","", "9515588585", 24324, "Master");
+    EXPECT_EQ(test.getId(), 561252);
+    EXPECT_EQ(test.getName(), "");
+    EXPECT_EQ(test.getAddress(), "");
+    EXPECT_EQ(test.getPhoneNumber(), 9515444343);
+    EXPECT_EQ(test.getPlanId(), 12222);
+    EXPECT_EQ(test.getPaymentMethod(), "Visa");
 }
 
 int main(int argc, char **argv) {
-    Customer *customer1 = new Customer(561252, "Arthur","", 9515444343, 12222, "Visa");
+    Customer customer1 = Customer(561252, "Arthur","", "9515444343", 12222, "Visa");
     testList.AddNewCustomer();
     ::testing::InitGoogleTest(&argc, argv);
     int val = RUN_ALL_TESTS();
-    delete customer1; delete customer2; delete staticCustomer; 
     allLists.clear();
     return val;
 }

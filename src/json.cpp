ee749d65c360c25c7f633f18929056e237f56dc4
Arman Seth <arman@Armans-MBP.lan>
2023-06-01 14:26:31 -0700

    sorting by plan and payment method

---
+++ src/json.cpp
@@ -0,0 +1,6 @@
+#include "rapidjson/document.h"     
+
+
+
+
+
---
+++ src/sort.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include "../header/customer.h"
+#include <iostream>
+using namespace std;
+
+
+class Sorter {
+public:
+    vector<Customer> sortedCustomers;
+    vector<Customer> sorByPlanID(const vector<Customer> items, int sortBy) {
+        if(sortBy == 1){
+            for (int i = 0; i < items.size(); i++) {
+                if (items.at(i).getPlanId() == sortBy) {
+                    sortedCustomers.push_back(items.at(i));
+                }
+            }
+        }else if(sortBy == 2){
+            for (int i = 0; i < items.size(); i++) {
+                if (items.at(i).getPlanId() == sortBy) {
+                    sortedCustomers.push_back(items.at(i));
+                }
+            }
+        }else if(sortBy == 3){
+            for (int i = 0; i < items.size(); i++) {
+                if (items.at(i).getPlanId() == sortBy) {
+                    sortedCustomers.push_back(items.at(i));
+                }
+            }
+        }else{
+            cout<<"No customers exist with plan " << sortBy << " please try again." ;
+        }
+       
+        return sortedCustomers;
+    }
+
+    vector<Customer> sortByPayment(const vector<Customer> items, string paymentM) {
+        if(paymentM == "mastercard"){
+            for (int i = 0; i < items.size(); i++) {
+                if (items.at(i).getPaymentMethod() == paymentM) {
+                    sortedCustomers.push_back(items.at(i));
+                }
+            }
+        }else if(paymentM == "visa"){
+            for (int i = 0; i < items.size(); i++) {
+                if (items.at(i).getPaymentMethod() == paymentM) {
+                    sortedCustomers.push_back(items.at(i));
+                }
+            }
+        }else if(paymentM == "amex"){
+            for (int i = 0; i < items.size(); i++) {
+                if (items.at(i).getPaymentMethod() == paymentM) {
+                    sortedCustomers.push_back(items.at(i));
+                }
+            }
+        }else if(paymentM == "cash"){
+            for (int i = 0; i < items.size(); i++) {
+                if (items.at(i).getPaymentMethod() == paymentM) {
+                    sortedCustomers.push_back(items.at(i));
+                }
+            }
+        }else if(paymentM == "check"){
+            for (int i = 0; i < items.size(); i++) {
+                if (items.at(i).getPaymentMethod() == paymentM) {
+                    sortedCustomers.push_back(items.at(i));
+                }
+            }
+        }else{
+            cout<<"No customers exist with " << paymentM << " payment method please try again." ;
+        }
+       
+        return sortedCustomers;
+    }
+};
+
+
+// // Example usage:
+// int main() {
+//     
+
+//     return 0;
+// }
